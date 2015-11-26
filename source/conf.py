557ca0b7ee1e8c3370ec34bd85081a01407f3b68
banga <dennisbanga@gmail.com>
2015-11-26 12:04:59 +0300

    updated the abbr list

--- source/conf.py
+++ source/conf.py
@@ -260,7 +260,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'jprpsystem', u'JPRP System',
+    (master_doc, 'jprpsystem', u'JPRP Documentation',
      [author], 1)
 ]
 
